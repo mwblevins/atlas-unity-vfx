f982e8819c7ee1f3f2c5a1ca66493ce25df5feb1
mwblevins <79066736+mwblevins@users.noreply.github.com>
2024-03-11 20:53:19 -0500

    New scripts for 8 levels

--- beat-detection/Assets/Scripts/AudioPeer.cs
+++ beat-detection/Assets/Scripts/AudioPeer.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class AudioPeer : MonoBehaviour
 {
     AudioSource _audioSource;
-    public float[] _samples = new float[512];
+    public static float[] _samples = new float[512];
+    public static float[] _freqBand = new float[8];
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +18,31 @@ public class AudioPeer : MonoBehaviour
     void Update()
     {
         GetSpectrumAudioSource ();
+        MakeFrequencyBands();
     }
     void GetSpectrumAudioSource()
     {
         _audioSource.GetSpectrumData (_samples, 0, FFTWindow.Blackman);
+    }
+    void MakeFrequencyBands()
+    {
+        float average = 0;
+        int count = 0;
+        for (int i =0; i < 8; i++)
+        {
+            int sampleCount = (int)Mathf.Pow (2, i) * 2;
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                    count++;
+            }
+            average /= count;
+
+            _freqBand[i] = average * 10;
+        }
     }
 }
